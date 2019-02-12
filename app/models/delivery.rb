87490b04fe01efd207724add71005c99afda2bbf
bburnett86 <breton.burnett@gmail.com>
2019-02-12 10:30:01 -0600

    Update model delivery model and add testing

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -11,12 +11,19 @@ class Delivery < ApplicationRecord
     CSV.read("lib/assets/delivery.csv")[1..-1].map{|p| Delivery.find_or_create_by(placement_id: p[0], date: Date.strptime(p[1], '%m/%d/%Y'), impressions: p[2])}
   end
 
-  def self.between_dates(start, finish)
+  def self.between_dates_impressions(start, finish)
     start_date = Date.strptime(start, '%m/%d/%Y')
     finish_date = Date.strptime(finish, '%m/%d/%Y')
-    total_impressions = Delivery.where(date: start_date..finish_date).inject(0){|sum,delivery| sum + delivery.impressions }
-    total_impression_cost = Delivery.where(date: start_date..finish_date).inject(0){|sum,delivery| sum + delivery.impressions.to_f/1000 * delivery.placement.cpm }
+    return Delivery.where(date: start_date..finish_date).inject(0){|sum,delivery| sum + delivery.impressions }
+  end
 
-    puts "Total (#{start}-#{finish}): #{number_with_delimiter(total_impressions)} impressions, $#{number_with_delimiter(total_impression_cost.round)}"
+  def self.between_dates_cost(start, finish)
+    start_date = Date.strptime(start, '%m/%d/%Y')
+    finish_date = Date.strptime(finish, '%m/%d/%Y')
+    return  (Delivery.where(date: start_date..finish_date).inject(0){|sum,delivery| sum + delivery.impressions.to_f/1000 * delivery.placement.cpm }).round
+  end
+
+  def self.between_dates_print(start, finish)
+    puts "Total (#{start}-#{finish}): #{number_with_delimiter(Delivery.between_dates_impressions(start, finish))} impressions, $#{number_with_delimiter(Delivery.between_dates_cost(start, finish))}"
   end
 end
